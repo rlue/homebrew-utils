4ce1a2114a344222e8deb9be29c8a1eb7ab40e09
Ryan Lue <ryan.lue@gmail.com>
2017-04-06 18:06:17 +0800

    Add sox dependency to formula

--- timer.rb
+++ timer.rb
@@ -1,10 +1,12 @@
 class Timer < Formula
-  desc "A pomodoro/interval timer"
-  homepage "https://github.com/rlue/timer"
-  url "https://github.com/rlue/timer/releases/download/v0.1.0/timer-0.1.0.tar.gz"
-  sha256 "0849381d347b27b494f3073d376aab712f3aab44daa7b6c68da62223c536eaad"
+  desc 'A pomodoro/interval timer'
+  homepage 'https://github.com/rlue/timer'
+  url 'https://github.com/rlue/timer/releases/download/v0.1.0/timer-0.1.0.tar.gz'
+  sha256 '0849381d347b27b494f3073d376aab712f3aab44daa7b6c68da62223c536eaad'
+
+  depends_on 'sox'
 
   def install
-    bin.install "timer"
+    bin.install 'timer'
   end
 end
