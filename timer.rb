6d7d44b106c6ed10900c4b072b38db271957274a
Ryan Lue <ryan.lue@gmail.com>
2017-05-26 17:45:15 +0800

    Update timer to release v0.1.1

--- timer.rb
+++ timer.rb
@@ -1,8 +1,8 @@
 class Timer < Formula
   desc 'A pomodoro/interval timer'
   homepage 'https://github.com/rlue/timer'
-  url 'https://github.com/rlue/timer/releases/download/v0.1.0/timer-0.1.0.tar.gz'
-  sha256 '0849381d347b27b494f3073d376aab712f3aab44daa7b6c68da62223c536eaad'
+  url 'https://github.com/rlue/timer/releases/download/v0.1.1/timer-0.1.1.tar.gz'
+  sha256 '36a1b3a56f8f8a3be4dc70e5376b1757fa02287c129d8643f9616b6807f1a31d'
 
   depends_on 'sox'
 
