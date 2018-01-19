46fcba841d75792cf8d9378c7aafab0a17bef70e
Ryan Lue <hello@ryanlue.com>
2018-01-19 21:13:01 +0800

    Version bump (0.1.2)

--- timer.rb
+++ timer.rb
@@ -1,12 +1,12 @@
 class Timer < Formula
-  desc 'A pomodoro/interval timer'
-  homepage 'https://github.com/rlue/timer'
-  url 'https://github.com/rlue/timer/releases/download/v0.1.1/timer-0.1.1.tar.gz'
-  sha256 '36a1b3a56f8f8a3be4dc70e5376b1757fa02287c129d8643f9616b6807f1a31d'
+  desc "Pomodoro/interval timer"
+  homepage "https://github.com/rlue/timer"
+  url "https://github.com/rlue/timer/releases/download/v0.1.2/timer-0.1.2.tar.gz"
+  sha256 "0a3293d46293ab352a5392a99279853c9e441566c30df2965f1bf292b7335089"
 
-  depends_on 'sox'
+  depends_on "sox"
 
   def install
-    bin.install 'timer'
+    bin.install "timer"
   end
 end
