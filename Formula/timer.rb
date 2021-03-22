b1d64214c29f5b1633549858ddf410c78bd6845d
Ryan Lue <hello@ryanlue.com>
2021-03-22 13:04:14 -0700

    Add License to timer formula

--- Formula/timer.rb
+++ Formula/timer.rb
@@ -3,6 +3,7 @@ class Timer < Formula
   homepage "https://github.com/rlue/timer"
   url "https://github.com/rlue/timer/releases/download/v0.1.3/timer-0.1.3.tar.gz"
   sha256 "fe5a18c9403a6837af5ba1cd74c1a038669f7b0228df0d499a23983905e53300"
+  license "MIT"
 
   depends_on "sox"
 
