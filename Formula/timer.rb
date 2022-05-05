a577c324e4e703bb00bec944655c06be62d3d9e9
Ryan Lue <hello@ryanlue.com>
2022-05-05 13:42:24 -0700

    Apply version bump to formula for `timer`

--- Formula/timer.rb
+++ Formula/timer.rb
@@ -1,8 +1,8 @@
 class Timer < Formula
   desc "Pomodoro/interval timer"
   homepage "https://github.com/rlue/timer"
-  url "https://github.com/rlue/timer/releases/download/v0.1.3/timer-0.1.3.tar.gz"
-  sha256 "fe5a18c9403a6837af5ba1cd74c1a038669f7b0228df0d499a23983905e53300"
+  url "https://github.com/rlue/timer/releases/download/v0.1.4/timer-0.1.4.tar.gz"
+  sha256 "0ac1889ea430cbac8f24d7b3cbcbf47d48fb30467b0fd4549e81870899fe5d5d"
   license "MIT"
 
   depends_on "sox"
